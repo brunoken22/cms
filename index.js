8e97f4af28734480acdafa7c4fb94fdc2a50c76e
brunoken22 <bruno_am_22@hotmail.com>
2022-11-02 15:19:17 -0300

    Actualizado

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-function main(){
-   const template = document.querySelector("#template")
-   const contenidos = document.querySelector(".contenidos")
+function mostrarResultados(datos) {
+   const template = document.querySelector("#template");
+   const contenidos = document.querySelector(".contenidos");
 
-   const clone = document.importNode(template.content,true)
-   contenidos.appendChild(clone)
+   for (let result of datos) {
+      const clone = document.importNode(template.content, true);
+      const articleImg = template.content.querySelector(".article-img");
+      const articleTitle = template.content.querySelector(".article-title");
+      contenidos.appendChild(clone);
+      contenidos.removeChild(contenidos.firstChild);
+      articleTitle.textContent = result.fields.appMisPelis;
+   }
+   console.log(contenidos);
 }
 
-main()
+function main() {
+   fetch(
+      "https://preview.contentful.com/spaces/dehbm7ub5p2i/environments/master/entries?access_token=Nc9wuQJ4_aK-N9OcKM8RKUNArEGoe_EUqrxeQ2e2s5Y"
+   )
+      .then(function (dato) {
+         return dato.json();
+      })
+
+      .then(function (result) {
+         mostrarResultados(result.items);
+      });
+}
+
+main();
